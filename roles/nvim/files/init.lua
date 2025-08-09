b27c19f0731e6b0091688c6a99aa6f052ad2caa8
Pesec1 <danzil344@gmail.com>
2025-08-09 14:40:08 +0500

    nvim: big config rewrite

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -1,42 +1,62 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.loaded_node_provider = 0
-vim.g.have_nerd_font = true
-
-vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
-vim.g.clipboard = 'osc52'
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.signcolumn = 'no'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.swapfile = false
-vim.opt.breakindent = true
-vim.opt.undofile = true
+vim.opt.wrap = false
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.background = 'dark'
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.winborder = "rounded"
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.scrolloff = 10
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
 
+vim.g.mapleader = " "
+
+vim.pack.add({
+	{ src = 'https://github.com/stevearc/oil.nvim.git' },
+	{ src = 'https://github.com/echasnovski/mini.pick.git' },
+	{ src = 'https://github.com/mason-org/mason.nvim.git' },
+	{ src = 'https://github.com/vague2k/vague.nvim' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+})
+
+require('nvim-treesitter').setup({
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+})
+
+require "vague".setup({ transparent = true })
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+
+require 'oil'.setup()
+vim.keymap.set('n', '-', ':Oil<Cr>')
+
+require 'mini.pick'.setup()
+vim.keymap.set('n', '<leader>sf', ':Pick files<Cr>')
+vim.keymap.set('n', '<leader><leader>', ':Pick buffers<Cr>')
+vim.keymap.set('n', '<leader>sg', ':lua MiniPick.builtin.grep_live()<Cr>')
+
+-- Lsp
+vim.lsp.enable({ "lua_ls", "ruff", "basedpyright" })
 vim.diagnostic.config { virtual_text = false, underline = false, signs = false }
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+require("mason").setup()
 
 -- Marks operations
 vim.keymap.set('n', '<leader>cm', '<cmd>delm!<CR>', { desc = 'Delete in file [m]arks' })
 vim.keymap.set('n', '<leader>cM', '<cmd>delm A-Z0-9<CR>', { desc = 'Delete file [M]arks' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -56,20 +76,3 @@ vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -5<CR>', { desc = 'Change 
 vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +5<CR>', { desc = 'Change size of split to right' })
 vim.keymap.set('n', '<C-Up>', '<cmd>horizontal resize +5<CR>', { desc = 'Change size of split to upper' })
 vim.keymap.set('n', '<C-Down>', '<cmd>horizontal resize -5<CR>', { desc = 'Change size of split to lower' })
-
--- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup {
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-  { 'numToStr/Comment.nvim', opts = {} },
-  { import = 'plugins' },
-  { import = 'autocmd' },
-}
--- vim: ts=2 sts=2 sw=2 et
--- roles/nvim/files/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'slugbyte/lackluster.nvim',
-  priority = 1000,
-  init = function()
-    vim.cmd.colorscheme 'lackluster-hack'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
